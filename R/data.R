c72eb626f21378dffa7e66e0ff13a4056c30ba6c
Singaram Natarajan <sin17m@csiro.au>
2024-03-01 05:39:42 +1100

    updated included data

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
-#' A data frame from a csv containing 4 XY coordinates and a id variable
+#' A data frame containing 4 XY coordinates and a id variable
 #'
-#' XY coordinates in CRS 4326 of a
+#' XY coordinates in WGS84 EPSG:4326 of a field experiment in Australia. Data was originally recorded in a csv format.
 #'
-#' @format ## `corners_dat_csv`
+#' @format
 #' A data frame with 4 rows and 3 columns:
 #' \describe{
 #'   \item{X}{Longitude}
@@ -11,4 +11,4 @@
 #'   ...
 #' }
 #' @source
-"corners_dat_csv"
+"cornersData"
