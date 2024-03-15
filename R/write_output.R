14a3476ad23a254b86f8483a64a4726a3ef81b66
Aswin Singaram Natarajan <sin17m@csiro.au>
2024-03-16 06:53:40 +1100

    changed name to be explicit

--- R/write_output.R
+++ R/write_output.R
@@ -2,6 +2,8 @@
 #'
 #' Write outputs as shapefile
 #'
+#' suppors most of the shapefile formats
+#'
 #' @param output_dat sf object to write to disk
 #' @param file_name file name with file path and extension
 #'
@@ -9,6 +11,6 @@
 #' @export
 #'
 #' @examples
-write_output <- function(output_dat, file_name){
+write_output_obj <- function(output_dat, file_name){
   st_write(obj = output_dat, dsn = file_name, delete_dsn=TRUE)
 }
