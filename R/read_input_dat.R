af178c944ffbec3ddff90c52d60ba5884e6bb627
Aswin Singaram Natarajan <sin17m@csiro.au>
2024-04-05 09:52:40 +1100

    users can pass coords in read

--- R/read_input_dat.R
+++ R/read_input_dat.R
@@ -1,8 +1,9 @@
 #' Read input data
 #'
 #' @param input_dat CSV file containing XY or any shape file formats.
-#' @param input_CRS Numeric input. CRS of input data as EPSG code
-#' @param to_CRS_M NUmeric input. Corresponding CRS in unit M to input CRS as EPSG code
+#' @param input_CRS Integer input. CRS of input data as EPSG code
+#' @param to_CRS_M Integer input. Corresponding CRS in unit M to input CRS as EPSG code
+#' @param XY a vector identifying colnames of X and Y e.g. c("Easting", "Northing")
 #'
 #' @return A sf object with CRS
 #' @export
@@ -14,19 +15,34 @@
 #'
 #' dat
 #'
-read_input_dat <- function(input_dat, input_CRS, to_CRS_M){
+read_input_dat <- function(input_dat, input_CRS, to_CRS_M, XY = NULL){
 
-  if ("data.frame" %in% class(input_dat)){
-    dat <- input_dat %>%
-      rename(X=1, Y=2) %>%
-      st_as_sf(coords = c("X","Y"))
-  } else if (tolower(substr(input_dat, nchar(input_dat)-2,nchar(input_dat))) == "csv") {
-    dat <- read.csv(input_dat)
-    colnames(dat)[1] <- "X"
-    colnames(dat)[2] <- "Y"
-    dat <- st_as_sf(dat, coords = c("X", "Y"))
+  if (!is.null(XY)){
+    if ("data.frame" %in% class(input_dat)){
+      dat <- input_dat %>%
+        rename(X=XY[1], Y=XY[2]) %>%
+        st_as_sf(coords = c("X","Y"))
+    } else if (tolower(substr(input_dat, nchar(input_dat)-2,nchar(input_dat))) == "csv") {
+      dat <- read.csv(input_dat)
+      dat <- dat %>%
+        rename(X=XY[1], Y=XY[2]) %>%
+        st_as_sf(coords = c("X", "Y"))
+    } else {
+      dat <- st_read(input_dat)
+    }
   } else {
-    dat <- st_read(input_dat)
+    if ("data.frame" %in% class(input_dat)){
+      dat <- input_dat %>%
+        rename(X=1, Y=2) %>%
+        st_as_sf(coords = c("X","Y"))
+    } else if (tolower(substr(input_dat, nchar(input_dat)-2,nchar(input_dat))) == "csv") {
+      dat <- read.csv(input_dat)
+      colnames(dat)[1] <- "X"
+      colnames(dat)[2] <- "Y"
+      dat <- st_as_sf(dat, coords = c("X", "Y"))
+    } else {
+      dat <- st_read(input_dat)
+    }
   }
 
   if (!is.na(st_crs(dat)$input)){
@@ -38,14 +54,14 @@ read_input_dat <- function(input_dat, input_CRS, to_CRS_M){
   } else if (missing(input_CRS)) {
     stop("Please provide value for input CRS")
   } else if (missing(to_CRS_M)) {
-    st_crs(dat) <- as.numeric(input_CRS)
+    st_crs(dat) <- input_CRS
     if (st_is_longlat(dat)) {
       dat <- st_transform(dat, 3857)
     } else {
-      st_crs(dat) <- as.numeric(input_CRS)
+      st_crs(dat) <- input_CRS
     }
   } else {
-    st_crs(dat) <- as.numeric(input_CRS)
+    st_crs(dat) <- input_CRS
     dat <- st_transform(dat, to_CRS_M)
   }
 
