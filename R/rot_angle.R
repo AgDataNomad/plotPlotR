9ac4db8fd613526bcb916aef3e7bdd48cb14bb27
Aswin Singaram Natarajan <sin17m@csiro.au>
2024-04-17 14:04:17 +1000

    bug fix

--- R/rot_angle.R
+++ R/rot_angle.R
@@ -29,7 +29,6 @@ rot_angle <- function(sf_object){
                      st_centroid() %>%
                      st_coordinates())
 
-
   corners_poly <- as.data.frame.matrix(corners_poly) %>%
     filter(X %in% c(min(X), max(X)) | Y %in% c(min(Y), max(Y))) %>%
     distinct(X,Y)
@@ -37,13 +36,13 @@ rot_angle <- function(sf_object){
   x_dist <- max(corners_poly$X)-min(corners_poly$X)
   y_dist <- max(corners_poly$Y)-min(corners_poly$Y)
 
-  if (x_dist<y_dist) {
-    corners_poly <- corners_poly %>%
-      arrange(Y, X)
-  } else {
-    corners_poly <- corners_poly %>%
-      arrange(X, Y)
-  }
+  # if (x_dist<y_dist) {
+  #   corners_poly <- corners_poly %>%
+  #     arrange(Y, X)
+  # } else {
+  #   corners_poly <- corners_poly %>%
+  #     arrange(X, Y)
+  # }
 
   a <- corners_poly[,1]
   b <- corners_poly[,2]
