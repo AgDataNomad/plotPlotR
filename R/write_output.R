70702af5373dd7223889ca6658d6b3fa104fcbe0
Aswin Singaram Natarajan <sin17m@csiro.au>
2024-05-08 12:34:17 +1000

    added option to overwrite file

--- R/write_output.R
+++ R/write_output.R
@@ -1,16 +1,27 @@
 #' Write outputs
 #'
-#' Write outputs as shapefile
-#'
-#' suppors most of the shapefile formats
+#' Write outputs of **plotPlotR** such as experimental layouts as shapefile; supports most shapefile formats
 #'
 #' @param output_dat sf object to write to disk
 #' @param file_name file name with file path and extension
+#' @param overwrite OPTIONAL, TRUE to overwrite file
 #'
 #' @return
 #' @export
 #'
 #' @examples
-write_output_obj <- function(output_dat, file_name){
-  st_write(obj = output_dat, dsn = file_name, delete_dsn=TRUE)
+write_output_obj <- function(output_dat, file_name, overwrite = NULL){
+
+  if (file.exists(file_name)){
+    if (is.null(overwrite)) {
+      stop("file already exist, use overwrite = TRUE to replace file")
+    } else if (overwrite == TRUE){
+        file.remove(file_name)
+        st_write(obj = output_dat, dsn = file_name)
+    } else if (overwrite  == FALSE){
+        stop("File already exists and overwrite is set to FALSE")
+      }
+  } else {
+    st_write(obj = output_dat, dsn = file_name)
+  }
 }
