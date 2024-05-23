d97ebb7fa36ea5b0e9b6bf1e3c5f07dd9b377dc5
Aswin Singaram Natarajan <sin17m@csiro.au>
2024-05-23 12:26:35 +1000

    changed arg name to something more meaningful

--- R/points_to_plots.R
+++ R/points_to_plots.R
@@ -1,10 +1,10 @@
 #' Make plots from points i.e. plot centers or use to adjust length and the width of plots in an existing plot layout
 #'
-#' @param sf_obj A sf object with geometry type POINTS referring to plot center or POLYGONS
+#' @param points_data A sf object with geometry type POINTS referring to plot center or POLYGONS
 #' @param plot_length Length of individual plots
 #' @param plot_width Width of individual plots
 #'
-#' @return A sf object of geometry type POLYGON with the same CRS as input sf_obj
+#' @return A sf object of geometry type POLYGON with the same CRS as input points_data
 #' @export
 #'
 #' @examples
@@ -26,14 +26,14 @@
 #' plot(adj_plots)
 #'
 #'
-make_plots <- function(sf_obj, plot_length, plot_width){
+make_plots <- function(points_data, plot_length, plot_width){
 
   expand_box_by <- plot_width/2
 
-  sf_obj_c <- sf_obj %>%
+  points_data_c <- points_data %>%
     st_centroid()
 
-  dgbb <- st_buffer(sf_obj_c,
+  dgbb <- st_buffer(points_data_c,
                     dist = expand_box_by,
                     endCapStyle = "SQUARE")
 
@@ -72,11 +72,13 @@ make_plots <- function(sf_obj, plot_length, plot_width){
     ungroup() %>%
     distinct_all()
 
-  rot_poly <- (st_geometry(dgbb_poly)-st_centroid(st_geometry(dgbb_poly)))*rot_shape(rot_angle(sf_obj))+st_centroid(st_geometry(dgbb_poly))
+  print(rad_2_deg(rot_angle(points_data)))
 
-  rot_poly <- cbind(st_as_sf(rot_poly), st_drop_geometry(sf_obj))
+  rot_poly <- rot_plots(dgbb_poly, rotation_angle = rad_2_deg(rot_angle(points_data)), selection = "plots")
 
-  st_crs(rot_poly) <- st_crs(sf_obj)
+  rot_poly <- cbind(st_as_sf(rot_poly), st_drop_geometry(points_data))
+
+  st_crs(rot_poly) <- st_crs(points_data)
 
   st_geometry(rot_poly) <- "geometry"
 
