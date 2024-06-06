109d7483d56ea31d2a39771786b84604ab51e5c9
TheSecondWin <snaswinkumar@gmail.com>
2024-06-06 10:53:33 +1000

    usethis imports

---
+++ R/plotPlotR-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom terra crop
+#' @importFrom terra mask
+#' @importFrom terra patches
+#' @importFrom terra sieve
+#' @importFrom terra vect
+## usethis namespace: end
+NULL
